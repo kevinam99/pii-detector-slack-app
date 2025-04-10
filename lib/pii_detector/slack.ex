29b31ea84ee857da4ccd8221e21aede064c4044f
Kevin <kevinam99@gmail.com>
2025-04-10 09:37:08 +0530

    Get file with redirect

--- lib/pii_detector/slack.ex
+++ lib/pii_detector/slack.ex
@@ -56,7 +56,7 @@ defmodule PiiDetector.Slack do
   end
 
   def fetch_file(file_url) do
-    case HTTPoison.get(file_url) do
+    case HTTPoison.get(file_url, follow_redirect: true) |> IO.inspect() do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         IO.inspect(body, label: "File content")
         {:ok, body}
